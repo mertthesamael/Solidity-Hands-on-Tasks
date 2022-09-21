aa7c538661966d1fc454dea6ca68b13c31e91604
Mert Enercan <79616834+mertthesamael@users.noreply.github.com>
2022-09-21 12:23:06 +0300

    Update Hands on Task - 1.sol

--- Hands on Task - 1.sol
+++ Hands on Task - 1.sol
@@ -21,7 +21,7 @@ contract FeeCollector { //0x128897683739e62E2e7875f80ab8218E300046cd
     // function for withdraw balance from contract to address (which is owner adress only).
     function withdraw(uint amount, address payable destAddr) public {
         require(msg.sender == owner, "Only owner can withdraw.");
-        require(amount <= balance, "icerde para kalmadi amk");
+        require(amount <= balance, "Insufficient balance");
         destAddr.transfer(amount);
         balance -= amount;
     }
